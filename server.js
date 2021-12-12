6a79b57776a5de7fed9f69248f1614da78a2bfb6
Tim Branyen <tim@tabdeveloper.com>
2021-12-11 20:47:22 -0800

    Clean up demo some, add comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-
 import { EventEmitter } from 'events';
 import { use, toString, innerHTML, html, release, createTree } from 'diffhtml';
 import 'diffhtml-components';
@@ -12,42 +11,46 @@ import App from './app.js';
 const ee = new EventEmitter();
 const app = express();
 
+// Reuse the web-worker middleware on the server, by default send is
+// postMessage, but that doesn't exist in Node. Instead use a simple event
+// emitter.
 use(workerTask({
   send: (...args) => ee.emit('deltas', ...args),
 }));
 
-app.get('/', (req, res, next) => {
-  res.sendFile(join(dirname(fileURLToPath(import.meta.url)), 'index.html'));
-});
-
+// Serve this directory for static files.
 app.use(express.static('.'));
 
 const server = app.listen(8000, () => {
   console.log('Listening on http://localhost:8000');
 });
 
-const wss = new WebSocketServer({
-  server,
-});
+// Reuse the express connection.
+const wss = new WebSocketServer({ server });
 
 wss.on('connection', ws => {
+  // Create a mount per connection, this will map to `<main></main>` in
+  // `index.html`.
   const mount = createTree(null);
+  // Generate a UUID to simulate a web worker threadId.
   const uuid = getUUID();
 
   let interval = null;
 
-  ee.on('deltas', deltas => {
-    ws.send(JSON.stringify(deltas));
-  });
+  // Whenever new deltas arrive, send them over the websocket. You could
+  // thereotically scope per user.
+  ee.on('deltas', deltas => ws.send(JSON.stringify(deltas)));
 
+  // Ensure cleanup whenever a socket is closed.
   ws.on('close', () => {
     release(mount);
     clearInterval(interval);
   });
 
+  // Initial render.
   innerHTML(mount, html`<${App} threadId=${uuid} />`);
 
-  // Start rendering.
+  // Re-render at an interval.
   interval = setInterval(() => {
     innerHTML(mount, html`<${App} threadId=${uuid} />`);
   }, 1000 / 60);
